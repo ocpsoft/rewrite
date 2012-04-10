f0c64fc1b7ff57380b5f7c32b9d345fb40ef8cfa
Christian Kaltepoth <christian@kaltepoth.de>
2012-04-10 07:26:28 +0200

    Integration test for FacesExpressionLanguageProvider

---
+++ integration-faces/src/test/java/org/ocpsoft/rewrite/faces/el/ExpressionLanguageBean.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2011 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.rewrite.faces.el;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ * @author Christian Kaltepoth
+ */
+@ManagedBean
+@RequestScoped
+public class ExpressionLanguageBean
+{
+
+   private String name;
+
+   private String uppercase;
+
+   public void action()
+   {
+      uppercase = name != null ? name.toUpperCase() : null;
+   }
+
+   public String getName()
+   {
+      return name;
+   }
+
+   public void setName(String name)
+   {
+      this.name = name;
+   }
+
+   public String getUppercase()
+   {
+      return uppercase;
+   }
+
+   public void setUppercase(String uppercase)
+   {
+      this.uppercase = uppercase;
+   }
+
+}
