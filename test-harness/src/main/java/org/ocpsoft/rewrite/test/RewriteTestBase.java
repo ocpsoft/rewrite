deae2eef0aaea4ba327bc069a4e9d7c772dbc76d
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2012-04-18 01:16:39 -0400

    Replaced deprecated method reference

--- test-harness/src/main/java/org/ocpsoft/rewrite/test/RewriteTestBase.java
+++ test-harness/src/main/java/org/ocpsoft/rewrite/test/RewriteTestBase.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,7 +43,7 @@ import com.sun.el.ExpressionFactoryImpl;
 
 /**
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
- * 
+ *
  */
 @RunWith(Arquillian.class)
 public class RewriteTestBase
@@ -89,7 +89,7 @@ public class RewriteTestBase
    {
       return DependencyResolvers.use(MavenDependencyResolver.class)
                .artifacts(coords)
-               .loadReposFromPom("pom.xml")
+               .loadMetadataFromPom("pom.xml")
                .resolveAs(GenericArchive.class);
    }
 
