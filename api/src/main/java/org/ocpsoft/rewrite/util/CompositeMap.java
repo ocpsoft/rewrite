838e7dc508d7315cc729a1de29678dca9b9c1ff3
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-03-08 13:36:15 -0500

    Added docs

--- api/src/main/java/org/ocpsoft/rewrite/util/CompositeMap.java
+++ api/src/main/java/org/ocpsoft/rewrite/util/CompositeMap.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ocpsoft.rewrite.util;
 
 import java.util.ArrayList;
@@ -9,6 +24,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * {@link Map} implementation supporting delegation to other {@link Map} instances configured with
+ * {@link CompositeMap#addDelegate(Map)}.
+ *
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ *
+ * @param <KEYTYPE>
+ * @param <VALUETYPE>
+ */
 public class CompositeMap<KEYTYPE, VALUETYPE> implements Map<KEYTYPE, VALUETYPE>
 {
    @SuppressWarnings("unchecked")
