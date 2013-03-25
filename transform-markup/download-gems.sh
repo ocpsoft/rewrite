2a604561ca2ee4abecd9fc7fbf47536ed787eb60
Christian Kaltepoth <christian@kaltepoth.de>
2013-03-25 19:46:56 +0100

    Added asciidoctor ruby lib

--- transform-markup/download-gems.sh
+++ transform-markup/download-gems.sh
@@ -6,12 +6,13 @@ mkdir -p $BASE/install
 mkdir -p $BASE/resources/ruby
 
 # install gems into install directory
-jruby -S gem install maruku sass RedCloth -i $BASE/install --no-rdoc --no-ri
+jruby -S gem install maruku sass RedCloth asciidoctor -i $BASE/install --no-rdoc --no-ri
 
 # copy required files to resource directory
 rsync -a --include '/lib/***' --exclude '*' $BASE/install/gems/maruku-*/ $BASE/resources/ruby/maruku
 rsync -a --include '/lib/***' --include '/VERSION*' --exclude '*' $BASE/install/gems/sass-*/ $BASE/resources/ruby/sass
 rsync -a --include '/lib/***' --exclude '*' $BASE/install/gems/RedCloth-*/ $BASE/resources/ruby/redcloth
+rsync -a --include '/lib/***' --exclude '*' $BASE/install/gems/asciidoctor-*/ $BASE/resources/ruby/asciidoctor
 
 echo "Done!"
 echo "You can now copy the contents of $BASE/resources to src/main/resources"
