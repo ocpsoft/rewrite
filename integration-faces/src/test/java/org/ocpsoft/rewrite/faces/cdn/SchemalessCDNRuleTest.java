f486a8501b859559d979e5a92b2b68f0475f7b27
Christian Kaltepoth <christian@kaltepoth.de>
2013-05-16 14:46:42 +0200

    Added failing test for #86 (currently ignored)

---
+++ integration-faces/src/test/java/org/ocpsoft/rewrite/faces/cdn/SchemalessCDNRuleTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2011 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.rewrite.faces.cdn;
+
+import static org.junit.Assert.assertThat;
+
+import org.apache.http.client.methods.HttpGet;
+import org.hamcrest.Matchers;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ocpsoft.rewrite.config.ConfigurationProvider;
+import org.ocpsoft.rewrite.faces.test.FacesBase;
+import org.ocpsoft.rewrite.test.HttpAction;
+import org.ocpsoft.rewrite.test.RewriteTestBase;
+
+@RunWith(Arquillian.class)
+public class SchemalessCDNRuleTest extends RewriteTestBase
+{
+
+   @Deployment(testable = false)
+   public static WebArchive getDeployment()
+   {
+      return FacesBase.getDeployment()
+               .addClass(SchemalessCDNRuleConfig.class)
+               .addAsServiceProviderAndClasses(ConfigurationProvider.class, SchemalessCDNRuleConfig.class)
+               .addAsWebResource(EmptyAsset.INSTANCE, "resources/jquery.js")
+               .addAsWebResource("schemaless-cdn-page.xhtml", "page.xhtml");
+   }
+
+   @Test
+   @Ignore // See: https://github.com/ocpsoft/rewrite/issues/86
+   public void shouldRewriteToSchemalessURL() throws Exception
+   {
+
+      HttpAction<HttpGet> action = get("/page.xhtml");
+
+      assertThat(action.getResponseContent(), Matchers.containsString(
+               "src=\"//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js\""));
+
+   }
+}
