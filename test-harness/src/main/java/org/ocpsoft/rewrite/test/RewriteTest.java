21adce7166da2e03acd8f9f5ce875bbfcd87bb65
Christian Kaltepoth <christian@kaltepoth.de>
2013-05-18 15:29:57 +0200

    Fixed integration test packaging problem with DispatcherTypeProvider

--- test-harness/src/main/java/org/ocpsoft/rewrite/test/RewriteTest.java
+++ test-harness/src/main/java/org/ocpsoft/rewrite/test/RewriteTest.java
@@ -206,13 +206,16 @@ public class RewriteTest extends RewriteTestBase
                .addAsResource(new File("../api-servlet/target/classes/org"))
 
                /*
-                * We have to manually create the META-NF/services entry for ServletRegistrationProvider,
-                * because the existing ones from the two modules overwrite each other when added to the
-                * archive.
+                * We have to manually create the META-NF/services entry for ServletRegistrationProvider
+                * and DispatcherTypeProvider, because the existing ones from the two modules overwrite 
+                * each other when added to the archive.
                 */
                .addAsServiceProvider("org.ocpsoft.rewrite.servlet.spi.ServletRegistrationProvider",
                         "org.ocpsoft.rewrite.servlet.impl.Servlet3ServletRegistrationProvider",
                         "org.ocpsoft.rewrite.servlet.impl.WebXmlServletRegistrationProvider")
+               .addAsServiceProvider("org.ocpsoft.rewrite.servlet.spi.DispatcherTypeProvider",
+                        "org.ocpsoft.rewrite.servlet.impl.Servlet3DispatcherTypeProvider",
+                        "org.ocpsoft.rewrite.servlet.impl.Servlet25DispatcherTypeProvider")
                .addAsResource(new File("../impl-servlet-2.5/target/classes/org"))
                .addAsResource(new File("../impl-servlet-2.5/target/classes/META-INF"))
                .addAsResource(new File("../impl-servlet-3.0/target/classes/org"))
