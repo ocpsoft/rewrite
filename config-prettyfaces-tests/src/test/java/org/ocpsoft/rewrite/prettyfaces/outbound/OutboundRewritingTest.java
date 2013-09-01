8794b401e5cf6f110fb749dac1f4880099526382
Christian Kaltepoth <christian@kaltepoth.de>
2013-09-01 12:01:34 +0200

    Added some tests for outbound rewriting with parameters (partially failing)

---
+++ config-prettyfaces-tests/src/test/java/org/ocpsoft/rewrite/prettyfaces/outbound/OutboundRewritingTest.java
@@ -0,0 +1,93 @@
+package org.ocpsoft.rewrite.prettyfaces.outbound;
+
+import static org.junit.Assert.assertThat;
+
+import java.net.URL;
+
+import org.hamcrest.Matchers;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ocpsoft.rewrite.prettyfaces.PrettyFacesTestBase;
+import org.ocpsoft.rewrite.test.RewriteTestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+@RunWith(Arquillian.class)
+public class OutboundRewritingTest extends RewriteTestBase
+{
+
+   @Deployment(testable = false)
+   public static WebArchive createDeployment()
+   {
+      return PrettyFacesTestBase.getDeployment()
+               .addAsWebResource("outbound/outbound.xhtml", "outbound.xhtml")
+               .addAsWebResource(EmptyAsset.INSTANCE, "no-param.xhtml")
+               .addAsWebResource(EmptyAsset.INSTANCE, "with-param.xhtml")
+               .addAsWebInfResource("outbound/outbound-pretty-config.xml", "pretty-config.xml");
+   }
+
+   @Drone
+   private WebDriver browser;
+
+   @ArquillianResource
+   private URL url;
+
+   /**
+    * Simple link for a mapping without any parameters.
+    */
+   @Test
+   public void outboundWithoutPathParam() throws Exception
+   {
+      browser.get(url + "/outbound.jsf");
+      String href = browser.findElement(By.id("no-param")).getAttribute("href");
+      assertThat(href, Matchers.endsWith("/no-param"));
+   }
+
+   /**
+    * The link contains a parameter that doesn't correspond to a path parameter. The URL should be rewritten and the
+    * query parameter should be added to the rewritten URL.
+    */
+   @Test
+   @Ignore
+   public void rewriteAdditionalQueryParam() throws Exception
+   {
+      browser.get(url + "/outbound.jsf");
+      String href = browser.findElement(By.id("no-param-plus-query")).getAttribute("href");
+      assertThat(href, Matchers.endsWith("/no-param?foo=bar"));
+   }
+
+   /**
+    * The link contains a parameter that corresponds to a path parameter in the mapping. The URL should be correctly
+    * rewritten.
+    */
+   @Test
+   @Ignore
+   public void rewriteWithPathParam() throws Exception
+   {
+      browser.get(url + "/outbound.jsf");
+      String href = browser.findElement(By.id("with-param")).getAttribute("href");
+      assertThat(href, Matchers.endsWith("/with-param/foobar"));
+   }
+
+   /**
+    * The link contains a parameter that corresponds to a path parameter in the mapping. There is also an additional
+    * query parameter, that doesn't correspond to any path parameter. This query parameter should be added to the
+    * rewritten URL.
+    */
+   @Test
+   @Ignore
+   public void rewriteWithPathParamAndQueryParam() throws Exception
+   {
+      browser.get(url + "/outbound.jsf");
+      String href = browser.findElement(By.id("with-param-plus-query")).getAttribute("href");
+      assertThat(href, Matchers.endsWith("/with-param/foobar?foo=bar"));
+   }
+
+}
