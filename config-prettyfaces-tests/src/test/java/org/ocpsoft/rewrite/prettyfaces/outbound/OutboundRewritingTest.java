3f71b1b0d9de73690a56202a48ae748f124caa6f
Christian Kaltepoth <christian@kaltepoth.de>
2013-10-26 10:25:42 +0200

    Ignore OutboundRewritingTest on Wildfly because of JAVASERVERFACES-3054

--- config-prettyfaces-tests/src/test/java/org/ocpsoft/rewrite/prettyfaces/outbound/OutboundRewritingTest.java
+++ config-prettyfaces-tests/src/test/java/org/ocpsoft/rewrite/prettyfaces/outbound/OutboundRewritingTest.java
@@ -12,13 +12,22 @@ import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.ocpsoft.rewrite.category.IgnoreForWildfly;
 import org.ocpsoft.rewrite.prettyfaces.PrettyFacesTestBase;
 import org.ocpsoft.rewrite.test.RewriteTestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+/**
+ * Ignored for Wildfly Beta1 because it ships with Mojarra 2.2.3 which ALWAYS appends 'jftfdi' and 'jffi' parameters.
+ * This seems to break our test asserts.
+ * 
+ * @see https://java.net/jira/browse/JAVASERVERFACES-3054
+ */
 @RunWith(Arquillian.class)
+@Category(IgnoreForWildfly.class)
 public class OutboundRewritingTest extends RewriteTestBase
 {
 
