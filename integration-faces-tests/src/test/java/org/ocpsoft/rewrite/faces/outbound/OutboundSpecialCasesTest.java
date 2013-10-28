a4e99e21d1896219217af2f3a5e4dedf85097ff6
Christian Kaltepoth <christian@kaltepoth.de>
2013-10-28 17:27:52 +0100

    Glassfish 4.0 appends JSESSIONID to JavaScript in links. So I disabled our tests checking this.

--- integration-faces-tests/src/test/java/org/ocpsoft/rewrite/faces/outbound/OutboundSpecialCasesTest.java
+++ integration-faces-tests/src/test/java/org/ocpsoft/rewrite/faces/outbound/OutboundSpecialCasesTest.java
@@ -22,7 +22,9 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.ocpsoft.rewrite.category.IgnoreForGlassfish4;
 import org.ocpsoft.rewrite.faces.annotation.RewriteFacesAnnotationsTest;
 import org.ocpsoft.rewrite.test.RewriteTest;
 import org.ocpsoft.rewrite.test.RewriteTestBase;
@@ -30,7 +32,18 @@ import org.ocpsoft.rewrite.test.RewriteTestBase;
 import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
+/**
+ * This test doesn't work on Glassfish 4.0 at all. For some reason Glassfish _always_ appends the JSESSIONID, even if
+ * the href just contains JavaScript. This leads to links like this:
+ * 
+ * <pre>
+ * href = &quot;javascript:void(0);jsessionid=fde6b122b14c03b5ad7dcf4c51b5&quot;
+ * </pre>
+ * 
+ * This can be reproduced even in a simple sample application without Rewrite.
+ */
 @RunWith(Arquillian.class)
+@Category(IgnoreForGlassfish4.class)
 public class OutboundSpecialCasesTest extends RewriteTestBase
 {
 
