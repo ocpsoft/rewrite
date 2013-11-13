7c283fd3e660559258fddb2e9513809f7c73a96e
Christian Kaltepoth <christian@kaltepoth.de>
2013-11-13 07:53:46 +0100

    Test to reproduce #147

---
+++ integration-faces-tests/src/test/java/org/ocpsoft/rewrite/faces/jsession/JSessionIdTest.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2011 <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ocpsoft.rewrite.faces.jsession;
+
+import static org.junit.Assert.assertThat;
+
+import java.net.URL;
+
+import org.hamcrest.Matchers;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ocpsoft.rewrite.faces.annotation.RewriteFacesAnnotationsTest;
+import org.ocpsoft.rewrite.test.RewriteTest;
+import org.ocpsoft.rewrite.test.RewriteTestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+
+@RunWith(Arquillian.class)
+public class JSessionIdTest extends RewriteTestBase
+{
+
+   @Deployment(testable = false)
+   public static WebArchive getDeployment()
+   {
+      return RewriteTest.getDeployment()
+               .addAsLibrary(RewriteFacesAnnotationsTest.getRewriteAnnotationArchive())
+               .addAsLibrary(RewriteFacesAnnotationsTest.getRewriteFacesArchive())
+               .addClass(JSessionIdBean.class)
+               .addAsWebResource("jsessionid.xhtml", "test.xhtml");
+   }
+
+   @ArquillianResource
+   private URL baseUrl;
+
+   @Test
+   public void withCookiesAndStandardOutcome() throws Exception
+   {
+
+      // GIVEN a browser with cookies enabled
+      WebDriver driver = createBrowserAndLoadPage(true);
+      assertThat(driver.getPageSource(), Matchers.not(Matchers.containsString("jsessionid")));
+
+      // WHEN a JSF action redirects with a standard outcome
+      driver.findElement(By.id("form:standardOutcome")).click();
+
+      // THEN the browser should be redirected
+      assertThat(driver.getCurrentUrl(), Matchers.containsString("redirected=true"));
+
+      // AND there should be no jsessionid in the URL
+      assertThat(driver.getCurrentUrl(), Matchers.not(Matchers.containsString("jsessionid")));
+
+   }
+
+   @Test
+   public void withCookiesAndNavigateOutcome() throws Exception
+   {
+
+      // GIVEN a browser with cookies enabled
+      WebDriver driver = createBrowserAndLoadPage(true);
+      assertThat(driver.getPageSource(), Matchers.not(Matchers.containsString("jsessionid")));
+
+      // WHEN a JSF action redirects with a standard outcome
+      driver.findElement(By.id("form:navigateOutcome")).click();
+
+      // THEN the browser should be redirected
+      assertThat(driver.getCurrentUrl(), Matchers.containsString("redirected=true"));
+
+      // AND there should be no jsessionid in the URL
+      assertThat(driver.getCurrentUrl(), Matchers.not(Matchers.containsString("jsessionid")));
+
+   }
+
+   @Test
+   // see: https://github.com/ocpsoft/rewrite/issues/147
+   @Ignore
+   public void withoutCookiesAndStandardOutcome() throws Exception
+   {
+
+      // GIVEN a browser with cookies disabled
+      WebDriver driver = createBrowserAndLoadPage(false);
+      assertThat(driver.getPageSource(), Matchers.containsString("jsessionid"));
+
+      // WHEN a JSF action redirects with a standard outcome
+      driver.findElement(By.id("form:standardOutcome")).click();
+
+      // THEN the browser should be redirected
+      assertThat(driver.getCurrentUrl(), Matchers.containsString("redirected=true"));
+
+      // AND there should be a jsessionid in the URL
+      assertThat(driver.getCurrentUrl(), Matchers.containsString("jsessionid"));
+
+   }
+
+   @Test
+   // see: https://github.com/ocpsoft/rewrite/issues/147
+   @Ignore
+   public void withoutCookiesAndNavigateOutcome() throws Exception
+   {
+
+      // GIVEN a browser with cookies disabled
+      WebDriver driver = createBrowserAndLoadPage(false);
+      assertThat(driver.getPageSource(), Matchers.containsString("jsessionid"));
+
+      // WHEN a JSF action redirects with a standard outcome
+      driver.findElement(By.id("form:navigateOutcome")).click();
+
+      // THEN the browser should be redirected
+      assertThat(driver.getCurrentUrl(), Matchers.containsString("redirected=true"));
+
+      // AND there should be a jsessionid in the URL
+      assertThat(driver.getCurrentUrl(), Matchers.containsString("jsessionid"));
+
+   }
+
+   private WebDriver createBrowserAndLoadPage(final boolean cookies)
+   {
+
+      // custom HtmlUnitDriver with cookies enabled or disabled
+      WebDriver driver = new HtmlUnitDriver() {
+         @Override
+         protected WebClient modifyWebClient(WebClient client)
+         {
+            client.getCookieManager().setCookiesEnabled(cookies);
+            return client;
+         }
+      };
+
+      // load the page twice to get rid of jsessionid if cookies are enabled
+      driver.get(baseUrl + "test");
+      driver.get(baseUrl + "test");
+
+      return driver;
+
+   }
+
+}
