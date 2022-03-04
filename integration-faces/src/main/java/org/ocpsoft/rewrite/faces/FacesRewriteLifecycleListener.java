d19987a4e6c0c573703d30024594dd031b12c7a0
Lars Grefer <github@larsgrefer.de>
2022-03-04 04:46:37 +0100

    Deprecate the DispatcherTypeProvider SPI

--- integration-faces/src/main/java/org/ocpsoft/rewrite/faces/FacesRewriteLifecycleListener.java
+++ integration-faces/src/main/java/org/ocpsoft/rewrite/faces/FacesRewriteLifecycleListener.java
@@ -1,17 +1,10 @@
 package org.ocpsoft.rewrite.faces;
 
-import java.util.Collections;
-import java.util.List;
-
+import javax.servlet.DispatcherType;
 import javax.servlet.http.HttpServletRequest;
 
-import org.ocpsoft.common.pattern.WeightedComparator;
-import org.ocpsoft.common.services.ServiceLoader;
-import org.ocpsoft.common.util.Iterators;
-import org.ocpsoft.rewrite.servlet.DispatcherType;
 import org.ocpsoft.rewrite.servlet.http.HttpRewriteLifecycleListener;
 import org.ocpsoft.rewrite.servlet.http.event.HttpServletRewrite;
-import org.ocpsoft.rewrite.servlet.spi.DispatcherTypeProvider;
 
 /**
  * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
@@ -21,8 +14,6 @@ public class FacesRewriteLifecycleListener extends HttpRewriteLifecycleListener
 {
    private static final String ORIGINAL_URL = FacesRewriteLifecycleListener.class.getName()
             + "_originalRequestURL";
-
-   private volatile List<DispatcherTypeProvider> dispatcherTypeProviders = null;
 
    @Override
    public void beforeInboundLifecycle(final HttpServletRewrite event)
@@ -38,32 +29,7 @@ public class FacesRewriteLifecycleListener extends HttpRewriteLifecycleListener
     */
    private DispatcherType getDispatcherType(final HttpServletRewrite event)
    {
-      for (DispatcherTypeProvider provider : getDispatcherTypeProviders()) {
-         DispatcherType dispatcherType = provider.getDispatcherType(event.getRequest(), event.getServletContext());
-         if (dispatcherType != null) {
-            return dispatcherType;
-         }
-      }
-      throw new IllegalStateException("Unable to determine dispatcher type of current request");
-   }
-
-   /**
-    * Returns the list of {@link DispatcherTypeProvider} implementations.
-    */
-   private List<DispatcherTypeProvider> getDispatcherTypeProviders()
-   {
-      List<DispatcherTypeProvider> result = dispatcherTypeProviders;
-      if (result == null) {
-         synchronized(this) {
-            result = dispatcherTypeProviders;
-            if (result == null) {
-               result = Iterators.asList(ServiceLoader.loadTypesafe(DispatcherTypeProvider.class).iterator());
-               Collections.sort(result, new WeightedComparator());
-               dispatcherTypeProviders = result;
-            }
-         }
-      }
-      return result;
+      return event.getRequest().getDispatcherType();
    }
 
    public static String getOriginalRequestURL(final HttpServletRequest request)
