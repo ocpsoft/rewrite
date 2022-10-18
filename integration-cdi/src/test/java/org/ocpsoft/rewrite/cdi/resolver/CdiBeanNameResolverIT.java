1791b2a85e4d62bd14a4986773b758d3a7ee65f1
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-10-19 00:35:52 +0200

    test(integration-cdi): fix ITs for CDI testing #345
    With CDI 4.0 the default for bean discovery switched to annotated
    beans only. It is necessary to switch back to the old default of
    scanning -all- classes.
    
    This means that any application using this library via CDI or
    Jakarta Faces will have to enable this, too!

--- integration-cdi/src/test/java/org/ocpsoft/rewrite/cdi/resolver/CdiBeanNameResolverIT.java
+++ integration-cdi/src/test/java/org/ocpsoft/rewrite/cdi/resolver/CdiBeanNameResolverIT.java
@@ -17,12 +17,13 @@ package org.ocpsoft.rewrite.cdi.resolver;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ocpsoft.rewrite.test.HttpAction;
 import org.ocpsoft.rewrite.test.RewriteIT;
+
+import java.io.File;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -37,8 +38,9 @@ public class CdiBeanNameResolverIT extends RewriteIT
    public static WebArchive getDeployment()
    {
       return RewriteIT.getDeployment()
-               .addAsWebInfResource(new StringAsset("<beans/>"), "beans.xml")
-               .addClasses(CdiBeanNameResolverBean.class, CdiBeanNameResolverConfigProvider.class);
+          // Necessary as of CDI 4.0 because we need bean-discovery-mode="all"
+          .addAsWebInfResource(new File("src/main/resources/META-INF/beans.xml"))
+          .addClasses(CdiBeanNameResolverBean.class, CdiBeanNameResolverConfigProvider.class);
    }
 
    @Test
