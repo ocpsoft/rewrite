c59459cd348a174fb2c0bf5edbb1f496fc37cbf4
Oliver Bertuch <o.bertuch@fz-juelich.de>
2022-10-18 23:33:31 +0200

    build: introduce Payara Arquillian Managed profile #345
    - Add Jakarta EE9 and EE10 compatible Payara versions
    - Add configuration to run Arquillian integration tests in a managed instance of Payara
    - Add JUnit category to skip tests that should be ignored on Payara

---
+++ test-base/src/main/java/org/ocpsoft/rewrite/category/IgnoreForPayara.java
@@ -0,0 +1,7 @@
+package org.ocpsoft.rewrite.category;
+
+/**
+ * JUnit category which will prevent a certain test to be executed when running the test suite with Payara.
+ */
+public interface IgnoreForPayara {
+}
